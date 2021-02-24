c650bef8f79e7dca436aef0f330a6a9dd65240fd
kk0055 <rtsw.lgy@gmail.com>
2021-02-24 13:37:34 +0800

    Route

--- routes/api.php
+++ routes/api.php
@@ -2,17 +2,14 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TaskController;
+use App\Http\Controllers\CategoryController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+Route::resource('/task', TaskController::class);
+
+Route::get('/category/{category}/tasks', [CategoryController::class, 'index']);
+Route::resource('/category', CategoryController::class);
+
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
