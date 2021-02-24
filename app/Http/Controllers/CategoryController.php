e20ef18e9dc3b078cbab5ccf0f3acc6213dcb4e0
kk0055 <rtsw.lgy@gmail.com>
2021-02-24 16:35:35 +0800

    Category controller

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -35,7 +35,12 @@ class CategoryController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $category = Category::create($request->only('name'));
+
+        return response()->json([
+            'status' => (bool) $category,
+            'message'=> $category ? 'Category Created' : 'Error Creating Category'
+        ]);
     }
 
     /**
@@ -46,40 +51,32 @@ class CategoryController extends Controller
      */
     public function show(Category $category)
     {
-        //
+        return response()->json($category);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Category  $category
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Category $category)
+    public function tasks(Category $category)
     {
-        //
+        return response()->json($category->tasks()->orderBy('order')->get());
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Category  $category
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Category $category)
-    {
-        //
-    }
+        public function update(Request $request, Category $category)
+        {
+            $status = $category->update($request->only('name'));
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Category  $category
-     * @return \Illuminate\Http\Response
-     */
+            return response()->json([
+                'status' => $status,
+                'message' => $status ? 'Category Updated!' : 'Error Updating Category'      
+            ]);
+        }
+
+   
     public function destroy(Category $category)
     {
-        //
+        $status  = $category->delete();
+
+        return response()->json([
+            'status' => $status,
+            'message' => $status ? 'Category Deleted' : 'Error Deleting Category'
+        ]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -7,13 +7,16 @@ use App\Http\Controllers\UserController;
 use App\Http\Controllers\CategoryController;
 
 
+Route::resource('/task', TaskController::class);
+Route::resource('/category', CategoryController::class);
+Route::get('/category/{category}/tasks', [CategoryController::class, 'index']);
 
 Route::post('login', [UserController::class, 'login']);
 Route::post('register', [UserController::class, 'register']);
 
 Route::group(['middleware' => 'auth:api'], function(){
-    Route::resource('/task', TaskController::class);
-    Route::resource('/category', CategoryController::class);
-    Route::get('/category/{category}/tasks', [CategoryController::class, 'index']);
-   
+       
 });
+
+
+
