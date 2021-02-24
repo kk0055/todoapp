e5919d5408541b14e89db9fcac1a88d1ae4dfe7f
kk0055 <rtsw.lgy@gmail.com>
2021-02-24 13:19:03 +0800

    migration increment

--- database/migrations/2021_02_24_044658_create_tasks_table.php
+++ database/migrations/2021_02_24_044658_create_tasks_table.php
@@ -14,7 +14,7 @@ class CreateTasksTable extends Migration
     public function up()
     {
         Schema::create('tasks', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id');
             $table->string('name');
             $table->unsignedInteger('category_id');
             $table->unsignedInteger('user_id');
